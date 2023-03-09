f231f7e308c447dd59dadb7055c07a0466a7b450
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-03-08 21:47:50 -0500

    cleaned

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function newImage(url, left, bottom){
     return object
 }
 
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
 function newItem(url, left, bottom){
     let object = newImage(url, left, bottom)
     object.addEventListener('dblclick', () => {
@@ -15,17 +22,6 @@ function newItem(url, left, bottom){
     })
 }
 
-newImage('assets/green-character.gif', 100, 100)
-newImage('assets/pine-tree.png', 450, 200)
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
 newItem('assets/sword.png', 500, 405)
 newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
-
-
-
-
-
